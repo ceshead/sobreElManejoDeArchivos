40e93ae5bcffb3c9cf0d316c5d7712e2c4f04619
ceshead <108100872+ceshead@users.noreply.github.com>
2024-02-03 19:14:49 -0500

    Add files via upload

---
+++ FileSystem2/mslearn-dotnet-files/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO; //desde la versión 6 no es necesario escribir estos 2 using
+using System.Collections.Generic; //Pero para recordar su uso las vamos a dejar
+
+//Este código pasa el nombre de la carpeta stores como la ubicación en la que buscar los archivos.
+var salesFiles = FindFiles("stores");//FindFiles es la función de abajo de tipo IEnumerable
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+//Creación una función denominada FindFiles que tome un parámetro folderName.
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+           salesFiles.Add(file);//carga la lista con un nuevo elemento string
+        }
+    }
+
+    return salesFiles;
+}
+
+
+
---
+++ FileSystem2/notas.md
@@ -0,0 +1,25 @@
+# EJEMPLO DE MANEJO DE ARCHIVOS CON C#
+
+## Basado en los archivos de mslearn-dotnet-files
+
+Este es un párrafo de texto simple. Puedes **enfatizar** palabras o *italicizar* texto fácilmente.
+
+### Listas
+
+- Elemento de lista 1
+- Elemento de lista 2
+  - Elemento anidado 1
+  - Elemento anidado 2
+
+### Enlaces
+
+[Enlace a Google](https://www.google.com)
+
+### Imágenes
+
+![Logo de Markdown](https://markdown-here.com/img/icon256.png)
+
+### Código
+
+```C#
+Console.WriteLine("Hola, mundo!")
